1aab0f3c923a310f4c56b41175b65297f8f0f179
Zeb Palmer <zeb@zebpalmer.com>
2016-05-14 16:28:28 -0600

    unfixing imports

--- mongoelector/__init__.py
+++ mongoelector/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 
 __author__ = 'Zeb Palmer'
 __email__ = 'zeb@zebpalmer.com'
 __version__ = '0.0.1'
 
-from locker import MongoLocker
-from elector import MongoElector
+from .locker import MongoLocker
+from .elector import MongoElector
