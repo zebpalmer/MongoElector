815feed558b490e013ba09a9b367d654554e5db0
Zeb Palmer <zeb@zebpalmer.com>
2016-05-14 18:08:20 -0600

    fixed license classifer

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
