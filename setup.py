995c0e5a9759f556c7ba243f236dce6336494a61
Zeb Palmer <zeb@clearwateranalytics.com>
2016-07-20 15:44:22 -0600

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='mongoelector',
-    version='0.1.5',
+    version='0.1.6',
     description="Distributed master election and locking in mongodb",
     long_description=readme + '\n\n' + history,
     author="Zeb Palmer",
