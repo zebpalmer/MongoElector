c771b1619f69068b7e0cef3360cf41f7d5a791e6
Zeb Palmer <zeb@zebpalmer.com>
2016-08-16 12:33:21 -0600

    starting elector logic

--- mongoelector/elector.py
+++ mongoelector/elector.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import threading
+from time import sleep
+
+from mongoelector.locker import MongoLocker, LockExists
 
 
 class MongoElector(object):
@@ -7,5 +11,93 @@ class MongoElector(object):
     election coolness
     """
 
-    def __init__(self):
-        """Does nothing. yet."""
+    def __init__(self, key, dbconn, dbname='mongoelector',
+                 ttl=15, onmaster=None, onmasterloss=None, onloop=None):
+        """Initial setup"""
+        self.shutdown = False
+        self._wasmaster = False
+        self.elector_thread = None
+        self.key = key
+        self.dbconn = dbconn
+        self.dbname = dbname
+        self.ttl = ttl
+        self.callback_onmaster = onmaster
+        self.callback_onmasterloss = onmasterloss
+        self.callback_onloop = onloop
+        self.mlock = MongoLocker(self.key, self.dbconn, dbname=self.dbname,
+                                 dbcollection='electorlocks', ttl=self.ttl, timeparanoid=True)
+
+    def start(self, blocking=False):
+        """Starts mongo elector polling"""
+        self.elector_thread = ElectorThread(self)  # give elector thread reference to mongolocker
+        self.elector_thread.start()
+        if blocking:
+            self.elector_thread.join()
+
+    def stop(self):
+        """Cleanly stop the elector. Surrender master if owned"""
+        self.shutdown = True
+        self.elector_thread.join()
+
+    @property
+    def running(self):
+        """Returns true if the elector logic is running"""
+        return self.elector_thread.isAlive()
+
+    @property
+    def ismaster(self):
+        """Returns True if this instance is master"""
+        return self.mlock.owned()
+
+    @property
+    def members(self):
+        """return list of members"""
+        raise NotImplementedError
+
+    @property
+    def master_exists(self):
+        """Returns a list of all instances that are availbe to become master"""
+        return self.mlock.locked()
+
+    def poll(self):
+        if self.mlock.owned():
+            self._wasmaster = True
+            self.mlock.touch()
+            if self.callback_onloop:
+                self.callback_onloop()
+        else:
+            if self._wasmaster:
+                if self.callback_onmasterloss:
+                    self.callback_onmasterloss()
+        if not self.master_exists:
+            try:
+                self.mlock.acquire(blocking=False)
+            except LockExists:
+                pass
+            else:
+                if self.mlock.owned():
+                    self._wasmaster = True
+                    if self.callback_onmaster:
+                        self.callback_onmaster()
+
+    def release(self):
+        """
+        releases master, if local instance owns it,
+        allowing other instances to become master
+        """
+        self.mlock.release()
+        sleep(2)  # let other instances fight it out
+
+
+class ElectorThread(threading.Thread):
+    """Calls the election polling logic"""
+
+    def __init__(self, elector):
+        super(ElectorThread, self).__init__()
+        self.elector = elector
+
+    def run(self):
+        """Main loop"""
+        while self.elector.shutdown is False:
+            self.elector.poll()
+            sleep(2)
--- tests/test_mongoelector.py
+++ tests/test_mongoelector.py
@@ -9,8 +9,10 @@ Tests for `mongoelector` module.
 """
 
 import unittest
-
+from time import sleep
+from pymongo import MongoClient
 from mongoelector import MongoElector
+from random import randint
 
 
 class TestMongoelector(unittest.TestCase):
@@ -18,15 +20,35 @@ class TestMongoelector(unittest.TestCase):
 
     def setUp(self):
         """setup unittests"""
-        pass
+        MongoClient().ml_unittest.electorlocks.drop()
+
 
     def tearDown(self):
         """teardown unittests"""
-        pass
+        MongoClient().ml_unittest.electorlocks.drop()
+
 
     def test_000_init(self):
         """Smoke test"""
-        MongoElector()
+        MongoElector('test_001_init', MongoClient())
+
+    def test_001_run(self):
+        m1 = MongoElector('test_001_run_' + str(randint(0,10000)), MongoClient(),
+                          dbname='ml_unittest',ttl=15)
+        m1.start()
+        c = 0
+        while c < 30 and m1.ismaster is False:
+            c += 1
+            sleep(1)
+        self.assertTrue(m1.ismaster)
+        m1.stop()
+        c = 0
+        while c < 30 and m1.ismaster is True:
+            c += 1
+            sleep(1)
+        self.assertFalse(m1.ismaster)
+
+
 
 
 if __name__ == '__main__':
